cc97cfe1ea41e56142886e058eacc27086fe64c9
PYChen <mmm198219@gmail.com>
2023-01-03 15:33:40 +0800

    add 2nd page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import tick from './ic-tick.svg';
 import './App.css';
 
 function App() {
+  const [isMain, setMain] = useState(true);
+  const toggleClass = () => {
+    setMain(!isMain);
+    console.log(isMain);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Confirm and Pay</h1>
-        <h2>PAY AMOUNT</h2>
-        <span className="Price">8.99 FCFA</span>
+      {/* className={isActive ? 'your_className': null}  */}
+      <header className={isMain ? "App-header" : "App-header second"}>
+        <img src={tick} className="tick" alt="tick" />
+        <h1 className='main'>Confirm and Pay</h1>
+        <h1 className='second'>Payment Success</h1>
+        <h2 className='main'>PAY AMOUNT</h2>
+        <h2 className='second'>TOTAL PRICE</h2>
+        <span className={isMain ? "Price" : "Price second"}>8.99 FCFA</span>
         <span className="PriceTo">to</span>
         <span className="TillID">123&nbsp;456</span>
-        <button type="button"> Pay </button>
+        <button type="button" onClick={toggleClass}> Pay </button>
       </header>
     </div>
   );
